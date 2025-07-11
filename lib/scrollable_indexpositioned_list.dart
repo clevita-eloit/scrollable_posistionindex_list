1b41b5cdc1c193fb9938240e45ef58b244358f57
Clevita <clevita@eloit.com>
2025-07-11 16:31:21 +0530

    first changes with scroll controller feature

---
+++ lib/scrollable_indexpositioned_list.dart
@@ -0,0 +1,10 @@
+ library scrollable_positioned_list_custom;
+
+/// A Calculator.
+// class Calculator {
+//   /// Returns [value] plus 1.
+//   int addOne(int value) => value + 1;
+// }
+export 'src/item_positions_listener.dart';
+export 'src/scrollable_positioned_list.dart';
+export 'src/scroll_offset_listener.dart';
